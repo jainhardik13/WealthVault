a970f11916183036535749dd17873f46e533131f
Hardik Jain <jainhardik130@gmail.com>
2026-02-11 20:32:27 +0530

    Phase 1 Completed, used NSE for webscraping data of indian stocks only.

--- app.py
+++ app.py
@@ -10,6 +10,7 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
 from models.portfolio_models import init_db
+from routes.portfolio_routes import portfolio_bp
 import os
 
 load_dotenv()
@@ -19,6 +20,8 @@ app.secret_key = os.getenv("SECRET_KEY")
 
 # Intialize the database
 init_db()
+
+app.register_blueprint(portfolio_bp)
 
 @app.route("/")
 def home():
